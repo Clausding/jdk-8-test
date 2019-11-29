68cb40951c0e322e9c26d083f305a933007c5db7
dingya <mexding@foxmai.com>
2019-11-29 10:46:41 +0800

    * 修改命名

---
+++ src/main/java/date_/Runner.java
@@ -0,0 +1,22 @@
+package date_;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+/**
+ * JDK8重新设计了日期、时间相关的类。优化了之前很差的设计。
+ */
+public class Runner {
+    public static void main(String args[]){
+        // 日期和时间
+        LocalDateTime dateTime = LocalDateTime.now();
+        System.out.printf("日期和时间：%s%n", dateTime);
+        // 日期
+        LocalDate date = dateTime.toLocalDate();
+        System.out.printf("日期：%s%n", date);
+        // 时间
+        LocalTime time = dateTime.toLocalTime();
+        System.out.printf("时间：%s%n", time);
+    }
+}
---
+++ src/main/java/lambda_/Runner.java
@@ -0,0 +1,21 @@
+package lambda_;
+
+import java.math.BigDecimal;
+
+/**
+ * lambda的用途：简化接口的实现方法的写法
+ */
+public class Runner {
+    public static void main(String args[]){
+        final int year = 2019;
+        Person farmer = (String name, BigDecimal money) -> System.out.printf("%s投资了%s年定期存款%s元%n", name, year, money);
+        Person businessman = (String name, BigDecimal money) -> System.out.printf("%s投资了%s年苹果股票%s元%n", name, year, money);
+        farmer.invest("二大爷", new BigDecimal("150000.00"));
+        businessman.invest("小黑", new BigDecimal("1500.00"));
+    }
+
+    interface  Person{
+        void invest(String name, BigDecimal money);
+    }
+
+}
